7ef60afd9a31adda95fd79293174c6df3fa7d351
ValipourReza <107016452+ValipourReza@users.noreply.github.com>
2026-09-01 10:29:38 +0330

    feat: add Spring Boot application

---
+++ src/main/java/ir/valipour/productbuilder/ProductBuilderApplication.java
@@ -0,0 +1,11 @@
+package ir.valipour.productbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductBuilderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductBuilderApplication.class, args);
+    }
+}
